cff1f7f73ed9980cca5b98ebade733e9f1a2b3a9
Muhammed Shan <muhammedshan930o@gmail.com>
2026-04-28 16:53:52 +0530

    implemented fetch current user slice

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import api from '../api/api'; 
 import { Loader2 } from 'lucide-react';
 import { useDispatch } from 'react-redux';
 import { logoutUser, fetchCurrentUser } from '../features/auth/store/authSlice';
